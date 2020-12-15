5a9791e67dc0c2b6513c6c4cc00360c1b4ceb819
windbell233 <1520057107@qq.com>
2020-12-15 21:01:34 +0800

    添加效果图，完成readme，项目结束

---
+++ Assets/BGM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip music;
+    private AudioSource back;
+    void Start()
+    {
+        back = this.GetComponent<AudioSource>();
+        back.loop = true; //设置循环播放  
+        back.volume = 0.9f;//设置音量最大，区间在0-1之间
+        back.clip = music;
+        back.Play(); //播放背景音乐，
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
